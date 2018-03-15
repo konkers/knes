01188101dc44247994f44bbdcaa4983727ad2369
Erik Gilling <konkers@konkers.net>
2018-03-14 23:55:28 -0700

    Hook all implemented blocks up to k6502.

--- 6502/k6502.sv
+++ 6502/k6502.sv
@@ -5,8 +5,18 @@ typedef struct packed {
 `else
 typedef struct {
 `endif
+    bit add_adl;
+    bit add_sb_6_0;
+    bit add_sb_7;
     bit adh_abh;
     bit adl_abl;
+    bit dl_adh;
+    bit dl_adl;
+    bit dl_db;
+    bit sb_x;
+    bit sb_y;
+    bit x_sb;
+    bit y_sb;
 } control_signals_t;
 
 // 6502 toplevel module.
@@ -36,23 +46,65 @@ wire [7:0] adh_bus;
 
 control_signals_t control_signals;
 
+// Address Bus High Register (ABH)
 register_single_in abh_reg(
     .data_in(adh_bus),
     .load(ph1 & control_signals.adh_abh),
     .data_out(a[15:8])
 );
 
+// Address Bus Low Register (ABL)
 register_single_in abl_reg(
     .data_in(adl_bus),
     .load(ph1 & control_signals.adl_abl),
     .data_out(a[7:0])
 );
 
+// X Index Register (X)
+register_index x_reg(
+    .data(sb_bus),
+    .load(control_signals.sb_x),
+    .bus_enable(control_signals.x_sb)
+);
+
+// Y Index Register (Y)
+register_index y_reg(
+    .data(sb_bus),
+    .load(control_signals.sb_y),
+    .bus_enable(control_signals.y_sb)
+);
+
+// Input Data Latch (DL)
+input_data_latch dl_reg(
+    .data_in(d),
+    .load(ph2),
+    .bus_enable0(control_signals.dl_db),
+    .data_out0(db_bus),
+    .bus_enable1(control_signals.dl_adl),
+    .data_out1(adl_bus),
+    .bus_enable2(control_signals.dl_adh),
+    .data_out2(adh_bus)
+);
+
+// Pre Decode Register (PD)
 wire [7:0] pre_decode_out;
-register_single_in pre_decode_reg(
+register_single_in pd_reg(
     .data_in(d),
     .load(ph2),
     .data_out(pre_decode_out)
+);
+
+wire [7:0] alu_out;
+
+// Adder Hold Register (ADD)
+register_adder_hold add_reg(
+    .data_in(alu_out),
+    .load(ph2),
+    .data_out0(adl_bus),
+    .bus_enable0(control_signals.add_adl),
+    .data_out1(sb_bus),
+    .bus_enable1_0_6(control_signals.add_sb_6_0),
+    .bus_enable1_7(control_signals.add_sb_7)
 );
 
 endmodule
--- Makefile
+++ Makefile
@@ -17,7 +17,10 @@ include build/verilator-sim.mk
 MODULE_NAME := k6502-test
 MODULE_SRCS := \
 	6502/clockgen.sv \
+	6502/input_data_latch.sv \
 	6502/k6502.sv \
+	6502/register_adder_hold.sv \
+	6502/register_index.sv \
 	6502/register_single_in.sv
 MODULE_TESTBENCH := 6502/k6502_test.cpp
 MODULE_TOP := k6502
